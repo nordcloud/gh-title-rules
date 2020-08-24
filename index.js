b06ed23c6b9f1fef1e15262de86bdfdeebc3b95b
Patryk Wieczorek <c3c3h3h3@gmail.com>
2020-08-24 17:27:54 +0200

    Init

---
+++ index.js
@@ -0,0 +1,14 @@
+import { setFailed, setOutput, getInput } from '@actions/core';
+import { context } from '@actions/github';
+
+try {
+    const title = String(context.payload.pull_request.title)
+    const regex = new RegExp(getInput('regex'))
+    const result = regex.exec(title)
+    const task = result[1]
+    console.log(task)
+    setOutput("task_number", task);
+
+} catch (error) {
+    setFailed(`Title must start with ticket number: [MCT-123] Text`);
+}
